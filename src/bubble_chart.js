7ab16eea32b585251e214546cd43aef82ee11450
Kai Magnus Müller <kai.mueller@hfg-gmuend.de>
2016-12-09 17:48:26 +0100

    Hmm

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -9,73 +9,71 @@
  *
  */
 function bubbleChart() {
-  // Constants for sizing
-  var width = 1440;
-  var height = 900;
+    // Constants for sizing
+    var width = 1440;
+    var height = 900;
 
-  // tooltip for mouseover functionality
-  var tooltip = floatingTooltip('gates_tooltip', 240);
+    // tooltip for mouseover functionality
+    var tooltip = floatingTooltip('gates_tooltip', 240);
 
-  // Locations to move bubbles towards, depending
-  // on which view mode is selected.
-  var center = { x: width / 2, y: height / 2 };
+    // Locations to move bubbles towards, depending
+    // on which view mode is selected.
+    var center = { x: width / 2, y: height / 2 };
 
-  var yearCenters = {
-    2008: { x: width / 3, y: height / 2 },
-    2009: { x: width / 2, y: height / 2 },
-    2010: { x: 2 * width / 3, y: height / 2 }
-  };
+    var yearCenters = {
+        2008: { x: width / 3, y: height / 2 },
+        2009: { x: width / 2, y: height / 2 },
+        2010: { x: 2 * width / 3, y: height / 2 }
+    };
 
-  // X locations of the year titles.
-  var yearsTitleX = {
-    2008: 160,
-    2009: width / 2,
-    2010: width - 160
-  };
+    // X locations of the year titles.
+    var yearsTitleX = {
+        2008: 160,
+        2009: width / 2,
+        2010: width - 160
+    };
 
-  // Used when setting up force and
-  // moving around nodes
-  var damper = 0.102;
+    // Used when setting up force and
+    // moving around nodes
+    var damper = 0.102;
 
-  // These will be set in create_nodes and create_vis
-  var svg = null;
-  var bubbles = null;
-  var nodes = [];
+    // These will be set in create_nodes and create_vis
+    var svg = null;
+    var bubbles = null;
+    var nodes = [];
 
-  // Charge function that is called for each node.
-  // Charge is proportional to the diameter of the
-  // circle (which is stored in the radius attribute
-  // of the circle's associated data.
-  // This is done to allow for accurate collision
-  // detection with nodes of different sizes.
-  // Charge is negative because we want nodes to repel.
-  // Dividing by 8 scales down the charge to be
-  // appropriate for the visualization dimensions.
-  function charge(d) {
-    return -Math.pow(d.radius, 2.0) / 8;
-  }
+    // Charge function that is called for each node.
+    // Charge is proportional to the diameter of the
+    // circle (which is stored in the radius attribute
+    // of the circle's associated data.
+    // This is done to allow for accurate collision
+    // detection with nodes of different sizes.
+    // Charge is negative because we want nodes to repel.
+    // Dividing by 8 scales down the charge to be
+    // appropriate for the visualization dimensions.
+    function charge(d) {
+        return -Math.pow(d.radius, 2.0) / 8;
+    }
 
-  // Here we create a force layout and
-  // configure it to use the charge function
-  // from above. This also sets some contants
-  // to specify how the force layout should behave.
-  // More configuration is done below.
-  var force = d3.layout.force()
-    .size([width, height])
-    .charge(charge)
-    .gravity(-0.01)
-    .friction(0.9);
+    // Here we create a force layout and
+    // configure it to use the charge function
+    // from above. This also sets some contants
+    // to specify how the force layout should behave.
+    // More configuration is done below.
+    var force = d3.layout.force()
+        .size([width, height])
+        .charge(charge)
+        .gravity(-0.01)
+        .friction(0.9);
 
 
-  // Nice looking colors - no reason to buck the trend
-  var fillColor = d3.scale.ordinal()
-    .domain(['low', 'medium', 'high'])
-    .range(['#005291', '#2881C7', '#5CB8FF']);
+    // Nice looking colors - no reason to buck the trend
+    var fillColor = '#2881C7';
 
-  // Sizes bubbles based on their area instead of raw radius
-  var radiusScale = d3.scale.pow()
-    .exponent(0.5)
-    .range([2, 85]);
+    // Sizes bubbles based on their area instead of raw radius
+    var radiusScale = d3.scale.pow()
+        .exponent(0.5)
+        .range([2, 85]);
 
   /*
    * This data manipulation function takes the raw data from
@@ -89,29 +87,26 @@ function bubbleChart() {
    * This function returns the new node array, with a node in that
    * array for each element in the rawData input.
    */
-  function createNodes(rawData) {
-    // Use map() to convert raw data into node data.
-    // Checkout http://learnjsdata.com/ for more on
-    // working with data.
-    var myNodes = rawData.map(function (d) {
-      return {
-        id: d.id,
-        radius: radiusScale(+d.total_amount),
-        value: d.total_amount,
-        name: d.grant_title,
-        org: d.organization,
-        group: d.group,
-        year: d.start_year,
-        x: Math.random() * 900,
-        y: Math.random() * 800
-      };
-    });
+    function createNodes(rawData) {
+        // Use map() to convert raw data into node data.
+        // Checkout http://learnjsdata.com/ for more on
+        // working with data.
+        var myNodes = rawData.map(function (d) {
+            return {
+                id: d.numericcode,
+                radius: radiusScale(+d.Year1950),
+                value: d.Year1950,
+                name: d.Location,
+                x: Math.random() * 900,
+                y: Math.random() * 800
+            };
+        });
 
-    // sort them to prevent occlusion of smaller nodes.
-    myNodes.sort(function (a, b) { return b.value - a.value; });
+        // sort them to prevent occlusion of smaller nodes.
+        myNodes.sort(function (a, b) { return b.value - a.value; });
 
-    return myNodes;
-  }
+        return myNodes;
+    }
 
   /*
    * Main entry point to the bubble chart. This function is returned
@@ -126,50 +121,50 @@ function bubbleChart() {
    * rawData is expected to be an array of data objects as provided by
    * a d3 loading function like d3.csv.
    */
-  var chart = function chart(selector, rawData) {
-    // Use the max total_amount in the data as the max in the scale's domain
-    // note we have to ensure the total_amount is a number by converting it
-    // with `+`.
-    var maxAmount = d3.max(rawData, function (d) { return +d.total_amount; });
-    radiusScale.domain([0, maxAmount]);
+    var chart = function chart(selector, rawData) {
+        // Use the max total_amount in the data as the max in the scale's domain
+        // note we have to ensure the total_amount is a number by converting it
+        // with `+`.
+        var maxAmount = d3.max(rawData, function (d) { return +d.Year1950; });
+        radiusScale.domain([0, maxAmount]);
 
-    nodes = createNodes(rawData);
-    // Set the force's nodes to our newly created nodes array.
-    force.nodes(nodes);
+        nodes = createNodes(rawData);
+        // Set the force's nodes to our newly created nodes array.
+        force.nodes(nodes);
 
-    // Create a SVG element inside the provided selector
-    // with desired size.
-    svg = d3.select(selector)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
+        // Create a SVG element inside the provided selector
+        // with desired size.
+        svg = d3.select(selector)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
 
-    // Bind nodes data to what will become DOM elements to represent them.
-    bubbles = svg.selectAll('.bubble')
-      .data(nodes, function (d) { return d.id; });
+        // Bind nodes data to what will become DOM elements to represent them.
+        bubbles = svg.selectAll('.bubble')
+            .data(nodes, function (d) { return d.id; });
 
-    // Create new circle elements each with class `bubble`.
-    // There will be one circle.bubble for each object in the nodes array.
-    // Initially, their radius (r attribute) will be 0.
-    bubbles.enter().append('circle')
-      .classed('bubble', true)
-      .attr('r', 0)
-      .attr('fill', function (d) { return fillColor(d.group); })
-      .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
-      .attr('stroke-width', 1)
-      // .on('mouseover', showDetail)
-      // .on('mouseout', hideDetail)
-      .on("click" , nodesAsButtons);
+        // Create new circle elements each with class `bubble`.
+        // There will be one circle.bubble for each object in the nodes array.
+        // Initially, their radius (r attribute) will be 0.
+        bubbles.enter().append('circle')
+            .classed('bubble', true)
+            .attr('r', 0)
+            // .attr('fill', function (d) { return fillColor(d.group); })
+            // .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+            .attr('stroke-width', 1)
+            // .on('mouseover', showDetail)
+            // .on('mouseout', hideDetail)
+            .on("click" , nodesAsButtons);
 
-    // Fancy transition to make bubbles appear, ending with the
-    // correct radius
-    bubbles.transition()
-      .duration(2000)
-      .attr('r', function (d) { return d.radius; });
+        // Fancy transition to make bubbles appear, ending with the
+        // correct radius
+        bubbles.transition()
+            .duration(2000)
+            .attr('r', function (d) { return d.radius; });
 
-    // Set initial layout to single group.
-    groupBubbles();
-  };
+        // Set initial layout to single group.
+        groupBubbles();
+    };
 
   /*
    * Sets visualization in "single group mode".
@@ -177,17 +172,17 @@ function bubbleChart() {
    * tick function is set to move all nodes to the
    * center of the visualization.
    */
-  function groupBubbles() {
-    // hideYears();
+    function groupBubbles() {
+        // hideYears();
 
-    force.on('tick', function (e) {
-      bubbles.each(moveToCenter(e.alpha))
-        .attr('cx', function (d) { return d.x; })
-        .attr('cy', function (d) { return d.y; });
-    });
+        force.on('tick', function (e) {
+            bubbles.each(moveToCenter(e.alpha))
+                .attr('cx', function (d) { return d.x; })
+                .attr('cy', function (d) { return d.y; });
+        });
 
-    force.start();
-  }
+        force.start();
+    }
 
   /*
    * Helper function for "single group mode".
@@ -203,12 +198,12 @@ function bubbleChart() {
    * its destination, and so allows other forces like the
    * node's charge force to also impact final location.
    */
-  function moveToCenter(alpha) {
-    return function (d) {
-      d.x = d.x + (center.x - d.x) * damper * alpha;
-      d.y = d.y + (center.y - d.y) * damper * alpha;
-    };
-  }
+    function moveToCenter(alpha) {
+        return function (d) {
+            d.x = d.x + (center.x - d.x) * damper * alpha;
+            d.y = d.y + (center.y - d.y) * damper * alpha;
+        };
+    }
 
   /*
    * Sets visualization in "split by year mode".
@@ -216,17 +211,17 @@ function bubbleChart() {
    * tick function is set to move nodes to the
    * yearCenter of their data's year.
    */
-  function splitBubbles() {
-    // showYears();
+    function splitBubbles() {
+        // showYears();
 
-    force.on('tick', function (e) {
-      bubbles.each(moveToYears(e.alpha))
-        .attr('cx', function (d) { return d.x; })
-        .attr('cy', function (d) { return d.y; });
-    });
+        force.on('tick', function (e) {
+            bubbles.each(moveToYears(e.alpha))
+                .attr('cx', function (d) { return d.x; })
+                .attr('cy', function (d) { return d.y; });
+        });
 
-    force.start();
-  }
+        force.start();
+    }
 
   /*
    * Helper function for "split by year mode".
@@ -242,70 +237,70 @@ function bubbleChart() {
    * its destination, and so allows other forces like the
    * node's charge force to also impact final location.
    */
-  function moveToYears(alpha) {
-    return function (d) {
-      var target = yearCenters[d.year];
-      d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
-      d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
-    };
-  }
+    function moveToYears(alpha) {
+        return function (d) {
+            var target = yearCenters[d.year];
+            d.x = d.x + (target.x - d.x) * damper * alpha * 1.1;
+            d.y = d.y + (target.y - d.y) * damper * alpha * 1.1;
+        };
+    }
 
   /*
    * Hides Year title displays.
    */
-  function hideYears() {
-    svg.selectAll('.year').remove();
-  }
+    function hideYears() {
+        svg.selectAll('.year').remove();
+    }
 
   /*
    * Shows Year title displays.
    */
-  function showYears() {
-    // Another way to do this would be to create
-    // the year texts once and then just hide them.
-    var yearsData = d3.keys(yearsTitleX);
-    var years = svg.selectAll('.year')
-      .data(yearsData);
+    function showYears() {
+        // Another way to do this would be to create
+        // the year texts once and then just hide them.
+        var yearsData = d3.keys(yearsTitleX);
+        var years = svg.selectAll('.year')
+            .data(yearsData);
 
-    years.enter().append('text')
-      .attr('class', 'year')
-      .attr('x', function (d) { return yearsTitleX[d]; })
-      .attr('y', 40)
-      .attr('text-anchor', 'middle')
-      .text(function (d) { return d; });
-  }
+        years.enter().append('text')
+            .attr('class', 'year')
+            .attr('x', function (d) { return yearsTitleX[d]; })
+            .attr('y', 40)
+            .attr('text-anchor', 'middle')
+            .text(function (d) { return d; });
+    }
 
 
   /*
    * Function called on mouseover to display the
    * details of a bubble in the tooltip.
    */
-  function showDetail(d) {
-    // change outline to indicate hover state.
-    d3.select(this).attr('stroke', 'black');
+    function showDetail(d) {
+        // change outline to indicate hover state.
+        d3.select(this).attr('stroke', 'black');
 
-    var content = '<span class="name">Title: </span><span class="value">' +
-                  d.name +
-                  '</span><br/>' +
-                  '<span class="name">Amount: </span><span class="value">$' +
-                  addCommas(d.value) +
-                  '</span><br/>' +
-                  '<span class="name">Year: </span><span class="value">' +
-                  d.year +
-                  '</span>';
-    tooltip.showTooltip(content, d3.event);
-  }
+        var content = '<span class="name">Title: </span><span class="value">' +
+            d.name +
+            '</span><br/>' +
+            '<span class="name">Amount: </span><span class="value">$' +
+            addCommas(d.value) +
+            '</span><br/>' +
+            '<span class="name">Year: </span><span class="value">' +
+            d.year +
+            '</span>';
+        tooltip.showTooltip(content, d3.event);
+    }
 
   /*
    * Hides tooltip
    */
-  function hideDetail(d) {
-    // reset outline
-    d3.select(this)
-      .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+    function hideDetail(d) {
+        // reset outline
+        d3.select(this)
+            .attr('stroke', d3.rgb(fillColor(d.group)).darker());
 
-    tooltip.hideTooltip();
-  }
+        tooltip.hideTooltip();
+    }
 
   /*
    * Externally accessible function (this is attached to the
@@ -314,17 +309,17 @@ function bubbleChart() {
    *
    * displayName is expected to be a string and either 'year' or 'all'.
    */
-  chart.toggleDisplay = function (displayName) {
-    if (displayName === true) {
-      splitBubbles();
-    } else {
-      groupBubbles();
-    }
-  };
+    chart.toggleDisplay = function (displayName) {
+        if (displayName === true) {
+            splitBubbles();
+        } else {
+            groupBubbles();
+        }
+    };
 
 
-  // return the chart function from closure.
-  return chart;
+    // return the chart function from closure.
+    return chart;
 }
 
 /*
@@ -339,11 +334,11 @@ var myBubbleChart = bubbleChart();
  * Calls bubble chart function to display inside #vis div.
  */
 function display(error, data) {
-  if (error) {
-    console.log(error);
-  }
+    if (error) {
+        console.log(error);
+    }
 
-  myBubbleChart('#vis', data);
+    myBubbleChart('#vis', data);
 }
 
 /*
@@ -371,12 +366,12 @@ function display(error, data) {
 // }
 var buttonId = false;
 function nodesAsButtons() {
-            console.log("test");
+    console.log("test");
 
-            buttonId = !buttonId;
-            // Toggle the bubble chart based on
-            // the currently clicked button.
-            myBubbleChart.toggleDisplay(buttonId);
+    buttonId = !buttonId;
+    // Toggle the bubble chart based on
+    // the currently clicked button.
+    myBubbleChart.toggleDisplay(buttonId);
 }
 
 /*
@@ -384,20 +379,20 @@ function nodesAsButtons() {
  * and add commas to it to improve presentation.
  */
 function addCommas(nStr) {
-  nStr += '';
-  var x = nStr.split('.');
-  var x1 = x[0];
-  var x2 = x.length > 1 ? '.' + x[1] : '';
-  var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
-    x1 = x1.replace(rgx, '$1' + ',' + '$2');
-  }
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
 
-  return x1 + x2;
+    return x1 + x2;
 }
 
 // Load the data.
-d3.csv('data/gates_money.csv', display);
+d3.csv('data/WorldPop_Sortiert.csv', display);
 
 // setup the buttons.
 // setupButtons();
